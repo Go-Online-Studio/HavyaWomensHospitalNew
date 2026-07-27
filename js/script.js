b880bae03b733473eed8a47c82501b4de3c960d2
Go-Online-Studio <shriitrackingsolutions86@gmail.com>
2026-07-27 16:42:33 +0530

    test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-
-
-// DOM Ready
-document.addEventListener("DOMContentLoaded", () => {
-  loadHeaderFooter(() => {
-    highlightActiveLink();
-    initHeaderBehavior();
-    initMobileNavToggle();
-    document.querySelector(".year").textContent = new Date().getFullYear();
-  });
-});
-
-// Load Header and Footer
-function loadHeaderFooter(callback) {
-         
-  document.getElementById("footer").innerHTML = `<div class="container">
+function loadHeaderFooter(_){document.getElementById("footer").innerHTML=`<div class="container">
         <div class="footWrap defaultPadding">
           <div class="row">
             <!-- Company Info -->
@@ -27,55 +12,55 @@ function loadHeaderFooter(callback) {
              
             </div>
             <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks">
-              <h5 class="fourthH">Quick Links</h5>
+              <h2 class="fourthH">Quick Links</h2>
               <ul>
                 <li>
                   <a class="footerLinks" href="index.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Home </a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Home </a>
                 </li>
                 <li>
                   <a class="footerLinks" href="havyaProfile.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>About Us</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>About Us</a>
                 </li>
                 <li>
                   <a class="footerLinks" href="meetOurDoctors.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Meet Our Doctors </a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Meet Our Doctors </a>
                 </li>
                 <li>
                   <a class="footerLinks" href="testimonials.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Patient Testimonials</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Patient Testimonials</a>
                 </li>
                 <li>
                   <a class="footerLinks" href="gallery.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Our Gallery</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Our Gallery</a>
                 </li>
                 <li>
                   <a class="footerLinks" href="happyMoments.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Our Happy Moments</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Our Happy Moments</a>
                 </li>
                 <li>
                   <a class="footerLinks" href="contactUs.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Contact</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg>Contact</a>
                 </li>
               </ul>
             </div>
             <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks1">
-              <h5 class="fourthH ">Our Treatments</h5>
+              <h2 class="fourthH ">Our Treatments</h2>
               <ul>   
-                 <li><a href="maternity.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Maternity</a></li>
-                 <li><a href="sonography.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Sonography</a></li>
-                 <li><a href="infertility.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Infertility</a></li>
-                 <li><a href="gynecology.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Gynecology</a></li>
-                 <li><a href="familyPlanning.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Family Planning</a></li>
-                 <li><a href="laparoscopy.html" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> Laparoscopy</a></li>
+                 <li><a href="maternity.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Maternity</a></li>
+                 <li><a href="sonography.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Sonography</a></li>
+                 <li><a href="infertility.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Infertility</a></li>
+                 <li><a href="gynecology.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Gynecology</a></li>
+                 <li><a href="familyPlanning.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Family Planning</a></li>
+                 <li><a href="laparoscopy.html" class="footerLinks"><svg aria-hidden="true" class="icon fa-solid-fa-circle-chevron-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm1 14.59L11.59 18L10.17 16.59L14.76 12l-4.59-4.59L11.59 6L17.59 12z"/></svg> Laparoscopy</a></li>
               </ul>
             </div>
             <div class="col-md-6 col-lg-auto mb-4 mb-md-0">
-              <h5 class="fourthH ">Contact Us</h5>
+              <h2 class="fourthH ">Contact Us</h2>
               <ul class="contact-info">
                 <li>
                   <a class="locationLink footerLinks" target="_blank" href="https://maps.app.goo.gl/qsc8AGfzZZnhsZ1H6">
-                    <i class="fa-solid fa-location-dot" aria-hidden="true"></i>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-location-dot" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 0 1 0-5a2.5 2.5 0 0 1 0 5z"/></svg>
                     <A1-TF-25>
                       <pre class="mb-0">
 2nd floor, Broadway Signature,
@@ -85,26 +70,26 @@ Vadodara, Gujarat 391101</pre>
                 </li>
                 <li>
                   <a href="tel:+916351836898" class="footerLinks">
-                    <i class="fa-solid fa-phone"></i>+91 63518 36898</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-phone" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>+91 63518 36898</a>
                 </li>
                 <li>
                   <a href="tel:+912653176295" class="footerLinks">
-                    <i class="fa-solid fa-phone"></i>+91 265 317 6295</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-phone" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>+91 265 317 6295</a>
                 </li>
                 <li>
                   <a class="emailAnchor footerLinks" href="mailto:havyawomenshospital@gmail.com">
-                    <i class="fa-solid fa-envelope" aria-hidden="true"></i>havyawomenshospital@gmail.com</a>
+                    <svg aria-hidden="true" class="icon fa-solid-fa-envelope" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5l-8-5V6l8 5l8-5v2z"/></svg>havyawomenshospital@gmail.com</a>
                 </li>
               </ul>
               <div class="social-icons mt-3">
-                <a target="_blank" class="facebook" href="https://www.facebook.com/people/Havya-womens-hospital/61574666012974/">
-                  <i class="fa-brands fa-facebook-f"></i>
+                <a target="_blank" class="facebook" href="https://www.facebook.com/people/Havya-womens-hospital/61574666012974/" aria-label="Facebook">
+                  <svg aria-hidden="true" class="icon fa-brands-fa-facebook fa-facebook-f" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89c1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12c0-5.523-4.477-10-10-10z"/></svg>
                 </a>
-                <a target="_blank" class="instagram" href="https://www.instagram.com/havyawomenshospital/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D#">
-                  <i class="fa-brands fa-instagram"></i>
+                <a target="_blank" class="instagram" href="https://www.instagram.com/havyawomenshospital/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D#" aria-label="Instagram">
+                  <svg aria-hidden="true" class="icon fa-brands-fa-instagram" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3z"/></svg>
                 </a>
-                <a class="whatsapp set-url-target" rel="noopener" data-mobile-target="" data-desktop-target="_blank" target="_blank" href="https://api.whatsapp.com/send?phone=916351836898">
-                  <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
+                <a class="whatsapp set-url-target" rel="noopener" data-mobile-target="" data-desktop-target="_blank" target="_blank" href="https://api.whatsapp.com/send?phone=916351836898" aria-label="WhatsApp">
+                  <svg aria-hidden="true" class="icon fa-brands-fa-whatsapp" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2 22l5.25-1.38c1.45.79 3.08 1.21 4.79 1.21c5.46 0 9.91-4.45 9.91-9.91S17.5 2 12.04 2m0 16.7c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.26 8.26 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c4.54 0 8.24 3.7 8.24 8.24s-3.7 8.24-8.24 8.24m4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.79.97c-.15.17-.3.19-.55.07c-.24-.12-1.05-.39-2-1.24c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43c.12-.14.17-.25.25-.41c.08-.17.04-.31-.02-.43c-.06-.12-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.45.06-.69.32c-.24.25-.92.9-.92 2.19s.94 2.54 1.07 2.72c.13.17 1.86 2.83 4.5 3.97c.63.27 1.12.43 1.5.55c.63.2 1.2.17 1.65.1c.5-.08 1.47-.6 1.68-1.18c.2-.58.2-.1.14-.17"/></svg>
                 </a>
               </div>
             </div>
@@ -127,91 +112,4 @@ Vadodara, Gujarat 391101</pre>
             </div>
           </div>
         </div>
-      </div> `; // Keep your current footer HTML here
-  callback?.();
-}
-
-// Highlight current page in navbar
-function highlightActiveLink() {
-  const current = (window.location.pathname.split("/").pop() || "index.html").split("?")[0];
-  document.querySelectorAll(".navLink").forEach(link => {
-    const href = link.getAttribute("href")?.split("?")[0];
-    if (!href || href === "#") return;
-    if (href === current) {
-      link.classList.add("active");
-      link.closest(".dropdownList")?.closest(".navLi")?.querySelector(".navLink")?.classList.add("active");
-    }
-  });
-}
-
-// Sticky Header on Scroll
-function initHeaderBehavior() {
-  const header = document.getElementById("main-header");
-  const belowContent = document.getElementById("headBelowContent");
-
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    const stickyStart = header.offsetTop + header.offsetHeight + 5;
-    const resetPoint = belowContent.offsetTop + belowContent.offsetHeight + 4;
-
-    if (scrollTop > stickyStart) {
-      header.classList.add("sticky-header", "visible");
-      header.classList.remove("headerAnimate");
-    } else if (scrollTop < resetPoint) {
-      header.classList.remove("sticky-header", "visible");
-      header.classList.add("headerAnimate");
-    }
-  };
-
-  window.addEventListener("scroll", handleScroll);
-  window.addEventListener("load", handleScroll);
-}
-
-// Mobile Nav Toggle and Submenu
-function initMobileNavToggle() {
-  const toggleBtn = document.querySelector(".navToggle");
-  const navMenu = document.querySelector(".navMenu");
-  const closeBtn = document.querySelector(".btn-nav-close");
-
-  toggleBtn?.addEventListener("click", () => navMenu.classList.toggle("active"));
-  closeBtn?.addEventListener("click", () => navMenu.classList.remove("active"));
-
-  document.querySelectorAll(".toggleSub").forEach(toggle => {
-    toggle.addEventListener("click", e => {
-      e.preventDefault();
-      const submenu = toggle.closest(".navLi").querySelector(".dropdownList");
-
-      // Close all others
-      document.querySelectorAll(".dropdownList.open").forEach(menu => {
-        if (menu !== submenu) menu.classList.remove("open");
-      });
-
-      submenu?.classList.toggle("open");
-    });
-  });
-}
-
-
-// WhatsApp URL Adjuster (Device-based Detection)
-(function () {
-  const mobileLink = "https://api.whatsapp.com/send?phone=916351836898";
-  const desktopLink = "https://web.whatsapp.com/send?phone=916351836898";
-
-  function isMobileDevice() {
-    return /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }
-
-  function updateWhatsAppLink() {
-    const isMobile = isMobileDevice();
-    const targetLink = isMobile ? mobileLink : desktopLink;
-
-    document.querySelectorAll(".set-url-target").forEach(el => {
-      el.setAttribute("href", targetLink);
-    });
-  }
-
-  window.addEventListener("resize", updateWhatsAppLink);
-  window.addEventListener("load", updateWhatsAppLink);
-})();
-
-
+      </div> `,_?.()}function highlightActiveLink(){let _=(window.location.pathname.split("/").pop()||"index.html").split("?")[0];document.querySelectorAll(".navLink").forEach(a=>{let e=a.getAttribute("href")?.split("?")[0];e&&"#"!==e&&e===_&&(a.classList.add("active"),a.closest(".dropdownList")?.closest(".navLi")?.querySelector(".navLink")?.classList.add("active"))})}function initHeaderBehavior(){let _=document.getElementById("main-header"),a=document.getElementById("headBelowContent"),e=()=>{let e=window.pageYOffset,t=_.offsetTop+_.offsetHeight+5,i=a.offsetTop+a.offsetHeight+4;e>t?(_.classList.add("sticky-header","visible"),_.classList.remove("headerAnimate")):e<i&&(_.classList.remove("sticky-header","visible"),_.classList.add("headerAnimate"))};window.addEventListener("scroll",e),window.addEventListener("load",e)}function initMobileNavToggle(){let _=document.querySelector(".navToggle"),a=document.querySelector(".navMenu"),e=document.querySelector(".btn-nav-close");_?.addEventListener("click",()=>a.classList.toggle("active")),e?.addEventListener("click",()=>a.classList.remove("active")),document.querySelectorAll(".toggleSub").forEach(_=>{_.addEventListener("click",a=>{a.preventDefault();let e=_.closest(".navLi").querySelector(".dropdownList");document.querySelectorAll(".dropdownList.open").forEach(_=>{_!==e&&_.classList.remove("open")}),e?.classList.toggle("open")})})}document.addEventListener("DOMContentLoaded",()=>{loadHeaderFooter(()=>{highlightActiveLink(),initHeaderBehavior(),initMobileNavToggle(),document.querySelector(".year").textContent=new Date().getFullYear()})}),function(){function _(){let _=/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),a=_?"https://api.whatsapp.com/send?phone=916351836898":"https://web.whatsapp.com/send?phone=916351836898";document.querySelectorAll(".set-url-target").forEach(_=>{_.setAttribute("href",a)})}window.addEventListener("resize",_),window.addEventListener("load",_)}();
